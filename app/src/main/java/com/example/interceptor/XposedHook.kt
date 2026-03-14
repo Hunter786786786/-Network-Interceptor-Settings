bb8dbe37b8847d50c693f0f43b34d35d7e1636c3
Hunter786786786 <arifmiah10050@gmail.com>
2026-03-14 20:55:30 +0300

    Update XposedHook.kt

--- app/src/main/java/com/example/interceptor/XposedHook.kt
+++ app/src/main/java/com/example/interceptor/XposedHook.kt
@@ -4,25 +4,14 @@ import android.app.Application
 import android.util.Log
 import com.highcapable.yukihookapi.YukiHookAPI
 import com.highcapable.yukihookapi.annotation.xposed.InjectYukiHookWithXposed
-import com.highcapable.yukihookapi.hook.factory.config
 import com.highcapable.yukihookapi.hook.factory.encase
 import com.highcapable.yukihookapi.hook.type.android.ApplicationClass
 import com.highcapable.yukihookapi.hook.xposed.proxy.IYukiHookXposedInit
-import de.robv.android.xposed.XSharedPreferences
-import org.json.JSONObject
 
 @InjectYukiHookWithXposed
 class XposedHook : IYukiHookXposedInit {
 
-    private lateinit var prefs: XSharedPreferences
-
-    override fun onInit() = YukiHookAPI.config {
-        isDebug = false
-    }
-
     override fun onHook() = encase {
-        prefs = XSharedPreferences("com.example.interceptor", "interceptor_prefs")
-        
         ApplicationClass.hook {
             injectMember {
                 method {
